ad4818268f4c63d9b9912fba00a3c4dadcc1367a
Shreya <goelshreya3001@gmail.com>
2026-01-31 17:07:18 +0530

    command lines

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.warn("This is warning");
+console.error("something went wrong");
+console.info("this is my first js code");
+console.table([{name:"A"},{name:"B"}]);
+console.time("loop");
+for(let i=0;i<10000;i++){}
+console.timeEnd("loop");
