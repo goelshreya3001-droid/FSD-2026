349918281c5a67fe8de371b84baf993cdb84f9a8
Shreya <goelshreya3001@gmail.com>
2026-03-20 12:25:00 +0530

    promise file

---
+++ ADVANCEJS/promise.js
@@ -0,0 +1,17 @@
+let promise=new Promise(function(resolve,reject){
+    let success=true;
+
+    if(success){
+        resolve("task done");
+
+    }
+    else{
+        reject("failed");
+    }
+});
+promise.then(function(result){
+    console.log(result);
+
+}).catch(function(error){
+    console.log(error);
+});
